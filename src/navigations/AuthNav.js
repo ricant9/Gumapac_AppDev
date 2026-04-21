5d490d7353de858022bda8f6bb643a021be891a1
ricant9 <ericagumapac38@gmail.com>
2026-04-21 14:24:46 +0800

    adding navigations

---
+++ src/navigations/AuthNav.js
@@ -0,0 +1,31 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { ROUTES } from '../utils';
+
+
+import Login from '../screens/auth/Login';
+import Register from '../screens/auth/Register';
+
+const Stack = createStackNavigator();
+
+const AuthNavigation = () => {
+  return (
+    <Stack.Navigator initialRouteName={ROUTES.LOGIN}>
+      <Stack.Screen
+        name={ROUTES.LOGIN}
+        component={Login}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen 
+        name={ROUTES.REGISTER} 
+        component={Register} 
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthNavigation;
