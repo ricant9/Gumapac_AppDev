9436a87518c5f1b3fafc652d7d0c011aea2395e2
ricant9 <ericagumapac38@gmail.com>
2026-04-21 14:25:51 +0800

    adding utils/utilities

---
+++ src/utils/routes.js
@@ -0,0 +1,14 @@
+const ROUTES = {
+  LOGIN: 'Login',
+  REGISTER: 'Register',
+  MAIN: 'Main',
+  HOME: 'Home',
+  PRODUCTS: 'Products',
+  PRODUCT_DETAIL: 'ProductDetail',
+  CART: 'Cart',
+  ORDERS: 'Orders',
+  ORDER_DETAIL: 'OrderDetail',
+  PROFILE: 'Profile',
+};
+
+export default ROUTES;
